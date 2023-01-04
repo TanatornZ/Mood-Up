57dd640c2aee5cbde249db396c13742a4d302f76
TanatornZ <bank250145.bb@gmail.com>
2023-01-04 11:06:22 +0700

    test merge

--- pages/register.tsx
+++ pages/register.tsx
@@ -4,6 +4,7 @@ function Register() {
   return (
     <div className='flex justify-center flex-col p-7'>
       <h1 className="mx-auto text-xl">กรุณากรอกข้อมูลเบื้องต้น</h1>
+      <h2>test</h2>
     </div>
   );
 }
