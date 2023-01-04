da4658b2466751ed1d23fe1421db076816c71f65
TanatornZ <bank250145.bb@gmail.com>
2023-01-04 11:04:49 +0700

    upload new file

---
+++ pages/first.tsx
@@ -0,0 +1,19 @@
+import { loadGetInitialProps } from "next/dist/shared/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function First() {
+  return (
+    <div className="flex justify-around items-center flex-col h-screen">
+      <div className="w-64 h-72 relative ">
+        <Image src={`/images/logo.png`} alt="logo" layout="fill" />
+      </div>
+      <button className="text-xl bg-secondary p-5 rounded-xl shadow-md">
+        <Link href="/register">เริ่มต้นใช้งาน</Link>
+      </button>
+    </div>
+  );
+}
+
+export default First;
---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div className='flex justify-center flex-col p-7'>
+      <h1 className="mx-auto text-xl">กรุณากรอกข้อมูลเบื้องต้น</h1>
+    </div>
+  );
+}
+
+export default Register;
