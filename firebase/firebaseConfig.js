217017d7fbb3d40941b9821c152b81af5001f86e
TanatornZ <bank250145.bb@gmail.com>
2023-01-15 22:37:20 +0700

    fix firebase config

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDn-ddXwMt66QpsERdVwj7IwlMEKKeme0A",
+  authDomain: "mood-up-ab8e1.firebaseapp.com",
+  projectId: "mood-up-ab8e1",
+  storageBucket: "mood-up-ab8e1.appspot.com",
+  messagingSenderId: "964782968837",
+  appId: "1:964782968837:web:aac6857f2079ee7b618c6c",
 };
 
 // Initialize Firebase
